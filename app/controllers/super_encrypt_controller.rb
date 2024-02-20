5299114fb946ae96b3ddc02165a0f56534ebf1da
wmk567 <13520020@std.stei.itb.ac.id>
2024-02-20 21:36:39 +0700

    bug fix

--- app/controllers/super_encrypt_controller.rb
+++ app/controllers/super_encrypt_controller.rb
@@ -10,7 +10,7 @@ class SuperEncryptController < ApplicationController
     message = ""
   
     padding_length = (num_cols - (user_input.length % num_cols)) % num_cols
-    user_input = user_input + 'Z' * padding_length
+    user_input = user_input + "\x00" * padding_length
 
     user_input.each_char do |char|
       encrypted_value = (char.ord + user_key[key_index].ord) % 256
@@ -18,13 +18,16 @@ class SuperEncryptController < ApplicationController
       key_index = (key_index + 1) % user_key.length
     end
 
-    split_text = message.scan(/.{1,#{num_cols}}/m)
-    grid = split_text.map { |row| row.chars }
-    grid_result = grid.transpose
-
     transpose_message = ""
-    grid_result.each do |row|
-      transpose_message += row.join('')
+    i = 0
+    while (i < num_cols)
+      j = 0
+      while(i+j < message.length)
+        position = i+j
+        transpose_message += message[position]
+        j+= num_cols
+      end
+      i+=1
     end
 
     encoded_message = Base64.strict_encode64(transpose_message)
@@ -39,23 +42,26 @@ class SuperEncryptController < ApplicationController
     num_cols = params[:numCols].to_i
     key_index = 0
     message = ""
-
+  
     padding_length = (num_cols - (user_input.length % num_cols)) % num_cols
     user_input = user_input + "\x00" * padding_length
-  
+
     user_input.each_char do |char|
       encrypted_value = (char.ord + user_key[key_index].ord) % 256
       message += (encrypted_value).chr
       key_index = (key_index + 1) % user_key.length
     end
 
-    split_text = message.scan(/.{1,#{num_cols}}/m)
-    grid = split_text.map { |row| row.chars }
-    grid_result = grid.transpose
-
     transpose_message = ""
-    grid_result.each do |row|
-      transpose_message += row.join('')
+    i = 0
+    while (i < num_cols)
+      j = 0
+      while(i+j < message.length)
+        position = i+j
+        transpose_message += message[position]
+        j+= num_cols
+      end
+      i+=1
     end
 
     send_data transpose_message, type: 'application/octet-stream', disposition: 'attachment'
@@ -69,13 +75,16 @@ class SuperEncryptController < ApplicationController
     message = ""
 
     num_rows = user_input.length / num_cols
-    split_text = user_input.scan(/.{1,#{num_rows}}/m)
-    grid = split_text.map { |row| row.chars }
-    grid_result = grid.transpose
-
     transpose_input = ""
-    grid_result.each do |row|
-      transpose_input += row.join('')
+    i = 0
+    while (i < num_rows)
+      j = 0
+      while(i+j < user_input.length)
+        position = i+j
+        transpose_input += user_input[position]
+        j+= num_rows
+      end
+      i+=1
     end
   
     transpose_input.each_char do |char|
@@ -84,6 +93,7 @@ class SuperEncryptController < ApplicationController
       key_index = (key_index + 1) % user_key.length
     end
   
+    message = message.gsub(/\x00+$/, '')
     encoded_message = Base64.strict_encode64(message)
     render json: { message: encoded_message }
   end
@@ -98,21 +108,24 @@ class SuperEncryptController < ApplicationController
     message = ""
 
     num_rows = user_input.length / num_cols
-    split_text = user_input.scan(/.{1,#{num_rows}}/m)
-    grid = split_text.map { |row| row.chars }
-    grid_result = grid.transpose
-
     transpose_input = ""
-    grid_result.each do |row|
-      transpose_input += row.join('')
+    i = 0
+    while (i < num_rows)
+      j = 0
+      while(i+j < user_input.length)
+        position = i+j
+        transpose_input += user_input[position]
+        j+= num_rows
+      end
+      i+=1
     end
-  
+
     transpose_input.each_char do |char|
       encrypted_value = (char.ord - user_key[key_index].ord) % 256
       message += (encrypted_value).chr
       key_index = (key_index + 1) % user_key.length
     end
-    
+
     message = message.gsub(/\x00+$/, '')
     send_data message, type: 'application/octet-stream', disposition: 'attachment'
   end
