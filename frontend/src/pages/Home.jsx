82a93633e149828d29edaa04fe43ffb5c0678688
iqbal <13520035@std.stei.itb.ac.id>
2024-02-21 00:25:37 +0700

    feat: add info at home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,63 @@
 const Home = () => {
   return (
     <>
-      <div className="w-96 mx-auto text-gray-400">
+      <div className="mx-auto text-gray-400">
         <h1 className="text-3xl font-extrabold mb-3">Classic Ciphers</h1>
         <div className="mb-8">
           <h2>William Manuel Kurniawan (13520020)</h2>
           <h2>Damianus Clairvoyance Diva Putra (13520035)</h2>
         </div>
-        <p className="text-sm">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni eum
-          possimus, voluptatum numquam, et dolorem voluptatibus nulla molestias
-          dolores sit atque facilis, praesentium odio aliquid provident nostrum
-          dicta. Quam, repellendus.
-        </p>
+        <table className="table-auto text-sm">
+          <thead>
+            <tr className="border-b">
+              <th className="pr-8">Ciphers</th>
+              <th className="pr-8">Input</th>
+              <th>Input Format</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="pr-8">Standard Vigenere Cipher</td>
+              <td className="pr-8">26-alphabet</td>
+              <td>text or txt file</td>
+            </tr>
+            <tr>
+              <td className="pr-8">Autokey Vigenere Cipher</td>
+              <td className="pr-8">26-alphabet</td>
+              <td>text or txt file</td>
+            </tr>
+            <tr>
+              <td className="pr-8">Extended Vigenere Cipher</td>
+              <td className="pr-8">256-ascii</td>
+              <td>text or any file</td>
+            </tr>
+            <tr>
+              <td className="pr-8">Playfair Cipher</td>
+              <td className="pr-8">26-alphabet</td>
+              <td>text or txt file</td>
+            </tr>
+            <tr>
+              <td className="pr-8">Affine Cipher</td>
+              <td className="pr-8">26-alphabet</td>
+              <td>text or txt file</td>
+            </tr>
+            <tr>
+              <td className="pr-8">Hill Cipher</td>
+              <td className="pr-8">26-alphabet</td>
+              <td>text or txt file</td>
+            </tr>
+            <tr>
+              <td className="pr-8">Superencrypt Cipher</td>
+              <td className="pr-8">256-ascii</td>
+              <td>text or any file</td>
+            </tr>
+            <tr>
+              <td className="pr-8">Enigma Cipher</td>
+              <td className="pr-8">26-alphabet</td>
+              <td>text or txt file or keyboard</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </>
   );
